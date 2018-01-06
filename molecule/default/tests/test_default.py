7e0017013b438023102146a310517d77c68a1bfc
donat-b <donatb09@gmail.com>
2018-01-06 08:27:04 +0000

    Support upgrading/downgrading

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,10 +5,18 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+version = '0.8.0'
+def test_restic(host):
+    f = host.file('/usr/local/bin/restic')
 
     assert f.exists
+    assert f.is_symlink
     assert f.user == 'root'
     assert f.group == 'root'
+
+    linked = f.linked_to
+    fl = host.file(f.linked_to)
+    assert linked == '/opt/restic/bin/restic-'+version
+    assert fl.user == 'root'
+    assert fl.group == 'root'
+    assert fl.mode == 0o755
